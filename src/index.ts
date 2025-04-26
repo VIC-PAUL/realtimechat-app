07a6a26ae97483019d01cf2e368b3c8741fbeed8
Victor Paul Choudhury <107463059+XKOBX@users.noreply.github.com>
2025-04-26 23:10:53 +0530

    changed port

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,121 @@
-import  {connection, server as WebSocketServer} from "websocket"
-import  http, { IncomingMessage } from 'http';
+import { connection, server as WebSocketServer } from "websocket";
+import http, { IncomingMessage } from "http";
 import { UserManager } from "./userManager";
-import { IncomingMessages, SupportedMessage } from "./messages/incomingMessages";
+import {
+  IncomingMessages,
+  SupportedMessage,
+} from "./messages/incomingMessages";
 import { InMemoryStore } from "./store/InMemoryStore";
-import {OutgoingMessages, SupportedMessage as OutgoingSupportedMessages } from "./messages/outgoingMessages";
+import {
+  OutgoingMessages,
+  SupportedMessage as OutgoingSupportedMessages,
+} from "./messages/outgoingMessages";
 
-const server = http.createServer(function(request : any, response:any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+const server = http.createServer(function (request: any, response: any) {
+  console.log(new Date() + " Received request for " + request.url);
+  response.writeHead(404);
+  response.end();
 });
 const userManager = new UserManager();
-const store = new InMemoryStore()
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+const store = new InMemoryStore();
+server.listen(8081, function () {
+  console.log(new Date() + " Server is listening on port 8081");
 });
 
 const wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: true,
 });
 
-function originIsAllowed(origin:string) {
+function originIsAllowed(origin: string) {
   // put logic here to detect whether the specified origin is allowed.
   return true;
 }
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-    
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            try{
-                messageHandler(connection,JSON.parse(message.utf8Data));
-            } catch(e){
+wsServer.on("request", function (request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log(
+      new Date() + " Connection from origin " + request.origin + " rejected."
+    );
+    return;
+  }
 
-            }
-            // console.log('Received Message: ' + message.utf8Data);
-            // connection.sendUTF(message.utf8Data);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+  var connection = request.accept("echo-protocol", request.origin);
+  console.log(new Date() + " Connection accepted.");
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      try {
+        messageHandler(connection, JSON.parse(message.utf8Data));
+      } catch (e) {}
+      // console.log('Received Message: ' + message.utf8Data);
+      // connection.sendUTF(message.utf8Data);
+    }
+  });
+  connection.on("close", function (reasonCode, description) {
+    console.log(
+      new Date() + " Peer " + connection.remoteAddress + " disconnected."
+    );
+  });
 });
 
-function messageHandler(ws:connection ,message: IncomingMessages){
-    if(message.type ==  SupportedMessage.JoinRoom){
-        const payload = message.payload;
-        userManager.addUser(payload.name, payload.userId, payload.roomId, ws)
+function messageHandler(ws: connection, message: IncomingMessages) {
+  if (message.type == SupportedMessage.JoinRoom) {
+    const payload = message.payload;
+    userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
+  }
+
+  if (message.type === SupportedMessage.SendMesssage) {
+    const payload = message.payload;
+    const user = userManager.getUser(payload.roomId, payload.userId);
+    if (!user) {
+      console.error("User not found in the db");
+      return;
     }
-
-    if(message.type=== SupportedMessage.SendMesssage){
-        const payload =  message.payload;
-        const user = userManager.getUser(payload.roomId,payload.userId);
-        if(!user){
-            console.error("User not found in the db");
-            return;
-        }
-        let chat = store.addChat(payload.userId,user.name,payload.roomId,payload.message);
-        if(!chat){
-            return;
-        }
-        const outgoingPayload: OutgoingMessages={
-            type: OutgoingSupportedMessages.AddChat,
-                payload:{
-                    chatId:chat.id,
-                    roomId:payload.roomId,
-                    message:payload.message,
-                    name:user.name,
-                    upvotes:0,
-                }
-            }
-
-            userManager.broadcast(payload.roomId,payload.userId,outgoingPayload)
-        }
-
-    if(message.type === SupportedMessage.UpvoteMessage){
-        const payload = message.payload
-        const chat = store.upvote(payload.userId,payload.roomId,payload.chatId);
-        if(!chat){
-            return;
-        }
-        const outgoingPayload: OutgoingMessages={
-            type: OutgoingSupportedMessages.UpdateChat,
-                payload:{
-                    chatId:payload.chatId,
-                    roomId:payload.roomId,
-                    upvotes:chat.upvotes.length,
-                }
-            }
-
-            userManager.broadcast(payload.roomId,payload.userId,outgoingPayload)
-        
+    let chat = store.addChat(
+      payload.userId,
+      user.name,
+      payload.roomId,
+      payload.message
+    );
+    if (!chat) {
+      return;
     }
+    const outgoingPayload: OutgoingMessages = {
+      type: OutgoingSupportedMessages.AddChat,
+      payload: {
+        chatId: chat.id,
+        roomId: payload.roomId,
+        message: payload.message,
+        name: user.name,
+        upvotes: 0,
+      },
+    };
+
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+
+  if (message.type === SupportedMessage.UpvoteMessage) {
+    const payload = message.payload;
+    const chat = store.upvote(payload.userId, payload.roomId, payload.chatId);
+    if (!chat) {
+      return;
+    }
+    const outgoingPayload: OutgoingMessages = {
+      type: OutgoingSupportedMessages.UpdateChat,
+      payload: {
+        chatId: payload.chatId,
+        roomId: payload.roomId,
+        upvotes: chat.upvotes.length,
+      },
+    };
+
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
 }
